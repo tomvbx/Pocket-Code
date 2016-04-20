fb2b804645f4a1a6654fda6d588541285ed7f99e
Hrvoje Lucic <hlucic@student.tugraz.at>
2016-04-20 12:12:13 +0200

    [HL,SC] Created new test class for programMenu

---
+++ catroidTest/src/org/catrobat/catroid/uitest/ui/menu/ProgramMenuRtlTest.java
@@ -0,0 +1,23 @@
+package org.catrobat.catroid.uitest.ui.menu;
+
+/**
+ * Created by Hrvoje on 20.4.2016..
+ */
+import org.catrobat.catroid.ProjectManager;
+import org.catrobat.catroid.R;
+import org.catrobat.catroid.content.Project;
+import org.catrobat.catroid.content.Script;
+import org.catrobat.catroid.content.Sprite;
+import org.catrobat.catroid.content.StartScript;
+import org.catrobat.catroid.content.bricks.NoteBrick;
+import org.catrobat.catroid.ui.MainMenuActivity;
+import org.catrobat.catroid.ui.dialogs.NewStringDialog;
+import org.catrobat.catroid.ui.fragment.FormulaEditorFragment;
+import org.catrobat.catroid.uitest.util.BaseActivityInstrumentationTestCase;
+import org.catrobat.catroid.uitest.util.UiTestUtils;
+public class ProgramMenuRtlTest extends BaseActivityInstrumentationTestCase<MainMenuActivity>  {
+
+    public ProgramMenuRtlTest() {
+        super(MainMenuActivity.class);
+    }
+}

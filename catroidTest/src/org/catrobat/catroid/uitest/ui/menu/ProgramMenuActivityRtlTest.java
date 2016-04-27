49a989291268100abc6aaf052d9bfbe071a92c1f
Hrvoje Lucic <hlucic@student.tugraz.at>
2016-04-27 20:40:22 +0200

    [HL] implemented first robotium test for program menu rtl

---
+++ catroidTest/src/org/catrobat/catroid/uitest/ui/menu/ProgramMenuActivityRtlTest.java
@@ -0,0 +1,48 @@
+package org.catrobat.catroid.uitest.ui.menu;
+
+import android.util.Log;
+
+import org.catrobat.catroid.R;
+import org.catrobat.catroid.ui.MainMenuActivity;
+import org.catrobat.catroid.ui.ProgramMenuActivity;
+import org.catrobat.catroid.uitest.util.UiTestUtils;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import com.robotium.solo.Solo;
+
+/**
+ * Created by Hrvoje on 27.4.2016..
+ */
+public class ProgramMenuActivityRtlTest extends ActivityInstrumentationTestCase2<MainMenuActivity> {
+    public ProgramMenuActivityRtlTest() {
+        super(MainMenuActivity.class);
+    }
+
+    private Solo mySolo;
+
+    public void setUp () throws Exception {
+        super.setUp ();
+        mySolo = new Solo( getInstrumentation (), getActivity ());
+    }
+    public void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+
+    public void testScriptButton() {
+
+      //  Log.d("Message", "test");
+//        int menuButtonLooksPaddingRight = getActivity().findViewById(R.id.program_menu_button_looks).getPaddingRight();
+        mySolo.goBackToActivity("MainMenuActivity");
+        mySolo.clickOnButton(getActivity().getString(R.string.main_menu_programs));
+        UiTestUtils.clickOnTextInList(mySolo, mySolo.getString(R.string.default_project_name));
+        UiTestUtils.clickOnTextInList(mySolo, mySolo.getString(R.string.default_project_cloud_sprite_name_1));
+        //mySolo.clickOnButton(getActivity().getString(R.string.default_project_cloud_sprite_name_1));
+       // mySolo.clickOnText(getActivity().getString(R.string.scripts));
+        UiTestUtils.clickOnTextInList(mySolo, mySolo.getString(R.string.scripts));
+
+
+
+    }
+}
--- catroidTest/src/org/catrobat/catroid/uitest/ui/menu/ProgramMenuActivityTest.java
+++ catroidTest/src/org/catrobat/catroid/uitest/ui/menu/ProgramMenuActivityTest.java
@@ -3,7 +3,9 @@ package org.catrobat.catroid.uitest.ui.menu;
 /**
  * Created by Hrvoje on 20.4.2016..
  */
+import android.test.ActivityInstrumentationTestCase2;
 import android.test.UiThreadTest;
+import android.util.Log;
 import android.widget.EditText;
 
 import org.catrobat.catroid.ProjectManager;
@@ -20,21 +22,21 @@ import org.catrobat.catroid.ui.dialogs.NewStringDialog;
 import org.catrobat.catroid.ui.fragment.FormulaEditorFragment;
 import org.catrobat.catroid.uitest.util.BaseActivityInstrumentationTestCase;
 import org.catrobat.catroid.uitest.util.UiTestUtils;
-public class ProgramMenuRtlTest extends BaseActivityInstrumentationTestCase<ProgramMenuActivity>  {
+public class ProgramMenuActivityTest extends ActivityInstrumentationTestCase2<ProgramMenuActivity> {
 
-    public ProgramMenuRtlTest() {
+    public ProgramMenuActivityTest() {
         super(ProgramMenuActivity.class);
     }
 
-    @UiThreadTest
-    public void testShouldNotCrashIfProjectIsNull() {
-        ProjectManager.getInstance().setProject(null);
-        getInstrumentation().callActivityOnPostCreate(getActivity(), null);
-        assertTrue("Test failed!", true);
+    public void tearDown() throws Exception {
+        super.tearDown();
+
     }
 
     public void testContinueButtonIsBiggerThanOthers() {
-        int menuButtonLooksPaddingRight = getActivity().findViewById(R.id.program_menu_button_looks).getPaddingRight();
+
+        Log.d("Message","test");
+//        int menuButtonLooksPaddingRight = getActivity().findViewById(R.id.program_menu_button_looks).getPaddingRight();
 
     }
 }

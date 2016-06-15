0c2a616017b37a9f02e1a596cbddd2d878567b80
sarehchoupani <sareh.choupani@student.tugraz.at>
2016-06-15 08:41:44 -0700

    show all HELP screens (step by step, tutorial,game design,etc.) in RTL mode. (not done)

--- catroidTest/src/org/catrobat/catroid/uitest/ui/activity/HelpScreenActivityTest.java
+++ catroidTest/src/org/catrobat/catroid/uitest/ui/activity/HelpScreenActivityTest.java
@@ -33,35 +33,21 @@ public class HelpScreenActivityTest extends ActivityInstrumentationTestCase2<Mai
 		super.tearDown();
 	}
 
-	public void testHelpScreens(){
+	public void testHelpScreens() {
 		solo.clickOnButton(getActivity().getString(R.string.main_menu_help));
 		solo.waitForView(solo.getView(R.id.webView));
 		solo.sleep(1000);
-		solo.clickOnWebElement(By.id("alice-tut-mobile"));      //game design
+		solo.clickOnWebElement(By.id("alice-tut-mobile"));
 		solo.sleep(1000);
 		solo.goBack();
-		//WebView object = (WebView) getActivity().findViewById((package name).R.id.(Webview ID));
-		//solo.clickOnWebElement(
-		solo.waitForWebElement(By.id("step-by-step-mobile"));
-		solo.goBack();
-		/*solo.clickOnWebElement(By.textConten
-		t("STEP-BY-STEP INTRO"));  //
-		solo.sleep(1000);
-		solo.goBack();
-		solo.clickOnWebElement(By.id("step-by-step-mobile")); //tutorials
-		solo.sleep(1000);
-		solo.goBack();
-		solo.clickOnWebElement(By.className("help-split"));   //
-		solo.sleep(1000);
-		solo.goBack();*/
 	}
+
 	public void testStarters() {
 		solo.clickOnButton(getActivity().getString(R.string.main_menu_help));
 		solo.waitForView(solo.getView(R.id.webView));
 		solo.sleep(1000);
 		solo.clickOnWebElement(By.className("help-mobile"));
-		solo.clickOnWebElement(By.id("step-by-step-mobile")); //tutorials
-
+		solo.clickOnWebElement(By.id("step-by-step-mobile"));
 		solo.goBack();
 
 	}
